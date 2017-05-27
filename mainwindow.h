387f0efbee367a30011958eb4519c89157925571
Orontion <orontion@gmail.com>
2017-05-27 14:37:19 +0300

    Добавлены комментарии в файл mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -15,15 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+//Слоты окна
 private slots:
 
-
+//Клик по кнопке "Добавить VLAN"
     void on_but_Add_clicked();
-
+//Действия при изменении выделения содержимого таблицы
     void on_table_VLAN_itemSelectionChanged();
-
+//Клик по кнопке удаления VLAN-а
     void on_but_Delete_clicked();
-
+//Выбор пункта меню "Файл -> Терминал..."
     void on_action_Term_triggered();
 
 private:
